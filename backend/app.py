67958dc47afbc17367e6894f75af284751c40bbb
Aswin Krishna M <maswink@gmail.com>
2025-11-08 14:27:15 +0100

    minor changes

--- backend/app.py
+++ backend/app.py
@@ -258,5 +258,5 @@ def handle_unregister(data):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8000))
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
